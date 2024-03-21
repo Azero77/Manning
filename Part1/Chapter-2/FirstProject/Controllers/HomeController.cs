6d7cbc32f67f304d57d951c849d4531687d297cb
Azero 77 <zaggar.azero.m@gmail.com>
2024-03-21 10:19:31 +0300

    Initial Commit

---
+++ Part1/Chapter-2/FirstProject/Controllers/HomeController.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace FirstProject.Controllers 
+{
+    public class HomeController : Controller
+    {
+        public IActionResult Index()
+        {
+            string Result = DateTime.Now.Hour < 12 ? "Good Morning" : "Good Evening";
+            return View("MyView" , Result);
+        }
+    }
+}
